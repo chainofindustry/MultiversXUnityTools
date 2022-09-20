61c81e0eec50b19d7bda99a4257ce0f3a1582d79
Ovidiu Arion <arion.ovidiu@hotmail.com>
2022-09-20 16:12:33 +0300

    Post + Get examples

--- Assets/ElrondUnityTools/Example/DemoScript.cs
+++ Assets/ElrondUnityTools/Example/DemoScript.cs
@@ -491,5 +491,47 @@ namespace ElrondUnityExamples
             }
         }
         #endregion
+
+        #region API
+        public void Get()
+        {
+            string url = "https://devnet-api.elrond.com/accounts/erd1jza9qqw0l24svfmm2u8wj24gdf84hksd5xrctk0s0a36leyqptgs5whlhf";
+            ElrondUnityTools.Manager.GetRequest(url, CompleteMethod);
+        }
+
+        private void CompleteMethod(ElrondUnityTools.OperationStatus operationStatus, string message, string resultJson)
+        {
+            if(operationStatus == ElrondUnityTools.OperationStatus.Complete)
+            {
+                Debug.Log(resultJson);
+            }
+            else
+            {
+                Debug.LogError(message + " " + resultJson);
+            }
+        }
+
+        public void Post()
+        {
+            //construct the url
+            string url = "https://devnet-gateway.elrond.com/transaction/cost";
+
+            //construct the params as a json string
+            string json = "{" +
+                          "\"nonce\":0," +
+                          "\"sender\":\"erd1lgp3ezf2wfkejnu0sm5y9g4x3ad05gr8lfc0g69vvdwwj0wjv0gscv2w4s\"," +
+                          "\"receiver\":\"erd1jza9qqw0l24svfmm2u8wj24gdf84hksd5xrctk0s0a36leyqptgs5whlhf\"," +
+                          "\"value\":\"1000000000000000\"," +
+                          "\"gasPrice\":1000000000," +
+                          "\"gasLimit\":89000," +
+                          "\"data\":\"WW91IHNlZSB0aGlzPw==\"," +
+                          "\"chainId\":\"D\"," +
+                          "\"version\":1," + "\"signature\":\"72ddcb105778051ea2a6f92b3869e2110d50f708708a2a3fe842014c062152c8aff78dae39868d97d25831915d3c60f4acfc749dfa8bdfa395f3769d2e231a05\"" +
+                          "}";
+
+            //Make the Post request 
+            ElrondUnityTools.Manager.PostRequest(url, json, CompleteMethod);
+        }
+        #endregion
     }
 }
