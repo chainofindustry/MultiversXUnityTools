b6ab19532d18026cbbbcf90edefcf538a4bfc636
Ovidiu Arion <arion.ovidiu@hotmail.com>
2022-10-11 15:58:15 +0300

    Full wallet connect sharp added

--- Assets/ExternalTools/WalletConnectUnity/Scripts/WalletConnectSharp/WalletConnectSharp.Core/Models/ValidJsonRpcRequestMethods.cs
+++ Assets/ExternalTools/WalletConnectUnity/Scripts/WalletConnectSharp/WalletConnectSharp.Core/Models/ValidJsonRpcRequestMethods.cs
@@ -6,8 +6,8 @@ namespace WalletConnectSharp.Core.Models
         public static readonly string EthFeeHistory = "eth_feeHistory";
         public static readonly string EthSendTransaction = "eth_sendTransaction";
         public static readonly string EthSendRawTransaction = "eth_sendRawTransaction";
-        public static readonly string EthSign = "erd_batch_sign";
-        public static readonly string EthSignTransaction = "erd_batch_sign";
+        public static readonly string EthSign = "eth_sign";
+        public static readonly string EthSignTransaction = "eth_signTransaction";
         public static readonly string EthSignTypedData = "eth_signTypedData";
         public static readonly string EthSignTypedDataV1 = "eth_signTypedData_v1";
         public static readonly string EthSignTypedDataV2 = "eth_signTypedData_v2";
